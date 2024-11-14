d12b95d01ccabf7ae62c2d430269a7a5140c9017
Ahmad Ijaz <mahmadijaz192@gmail.com>
2024-11-14 10:57:47 +0500

    Login Class

--- src/lab12/Main.java
+++ src/lab12/Main.java
@@ -3,7 +3,7 @@ package lab12;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the application!");
-		//greetUser();
+		greetUser();
 	}
 
 	public static void greetUser() {
