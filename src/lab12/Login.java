cd3e739da916c522600dead3cdb29882cc2060f5
Ahmad Ijaz <mahmadijaz192@gmail.com>
2024-11-14 11:00:47 +0500

    Experimental Commit 2

--- src/lab12/Login.java
+++ src/lab12/Login.java
@@ -2,6 +2,10 @@ package lab12;
 
 public class Login {
 	public boolean authenticate(String username, String password) {
+		if (username == null || password == null) {
+		System.out.println("Invalid input");
+		return false;
+		}
 		System.out.println("Authenticating user...");
 		return username.equals("admin") && password.equals("password123");
 		}
